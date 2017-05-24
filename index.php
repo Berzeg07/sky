c6b1e91c6a19442749798c6e1ded4776111f967c
Berzeg07 <berezg86@gmail.com>
2017-05-25 00:24:46 +0400

    29.14

--- index.php
+++ index.php
@@ -8,10 +8,200 @@
 	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
 	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
 	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/fonts.css">
 </head>
 <body>
 
+	<div class="header__wrap">
+		<div class="header__wrap_top">
 
+			<div class="container">
+				<div class="row">
+					<div class="header">
+
+						<div class="header__lang">
+							<div class="header__bars">
+								<i class="fa fa-bars" aria-hidden="true"></i>
+							</div>
+							<a class="lang_active" href="#">RU</a>
+							<a href="#">EN</a>
+						</div>
+						
+						<div class="header__menu-box">
+							<div class="col-xs-12 col-sm-12 col-md-6 no-padding">
+								<div class="header__tel">
+									<div class="header__tel_box"><a href="#"><img src="img/logo.png" alt=""></a></div>
+									<div class="header__tel_box header__tel_bottom"><span>8 800 800 80 80</span></div>
+									<div class="clearfix"></div>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-12 col-md-6 no-padding">
+								<ul class="header__menu">
+									<li><a href="#">главная</a></li>
+									<li><a href="#">семья</a></li>
+									<li><a href="#">друзья</a></li>
+									<li><a href="#">семьи</a></li>
+									<li><a href="#">контакты</a></li>
+								</ul>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="header__bottom">
+			<div class="container">
+				<span>Welcome</span>
+				<p>Главная страница сайта представляет собой лицо компании, дверь, через которую к вам входят клиенты. </p>
+			</div>	
+		</div>
+	</div>
+
+	<!-- end header -->
+
+	<!-- logo block -->
+	<div class="container dd">
+		<div class="row">
+
+			<div class="col-xs-12 col-sm-12 col-md-3 no-padding">
+				<a href="#">
+					<div class="block-logo block-logo_1">
+						<img class="img-responsive" src="img/im1.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="block-logo block-logo_2">
+						<img class="img-responsive" src="img/im2.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="block-logo block-logo_2">
+						<img class="img-responsive" src="img/im2.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+
+			<!-- 2 ===================== -->
+
+			<div class="col-xs-12 col-sm-12 col-md-3 no-padding">
+				<a href="#">
+					<div class="block-logo block-logo_2">
+						<img class="img-responsive" src="img/im2.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="block-logo block-logo_1">
+						<img class="img-responsive" src="img/im1.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="block-logo block-logo_3">
+						<img class="img-responsive" src="img/im3.jpg" alt="">
+						<div class="block-logo__hover block-logo__hover_3">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="block-logo block-logo_3">
+						<img class="img-responsive" src="img/im3.jpg" alt="">
+						<div class="block-logo__hover block-logo__hover_3">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+
+			<!-- 3 =================== -->
+
+			<div class="col-xs-12 col-sm-12 col-md-3 no-padding">
+				<a href="#">
+					<div class="block-logo block-logo_2">
+						<img class="img-responsive" src="img/im4.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="block-logo block-logo_1">
+						<img class="img-responsive" src="img/im1.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="block-logo block-logo_3">
+						<img class="img-responsive" src="img/im5.jpg" alt="">
+						<div class="block-logo__hover block-logo__hover_3">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="block-logo block-logo_3">
+						<img class="img-responsive" src="img/im5.jpg" alt="">
+						<div class="block-logo__hover block-logo__hover_3">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+			
+			<!-- 4 ===================== -->
+
+			<div class="col-xs-12 col-sm-12 col-md-3 no-padding">
+				<a href="#">
+					<div class="block-logo block-logo_4">
+						<img class="img-responsive" src="img/im6.jpg" alt="">
+						<div class="block-logo__hover block-logo__hover_3">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+
+				<a href="#">
+					<div class="block-logo block-logo_2">
+						<img class="img-responsive" src="img/im4.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+
+				<a href="#">
+					<div class="block-logo block-logo_2">
+						<img class="img-responsive" src="img/im4.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+	<!-- end logo block -->
 
 
 
--- js/common.js
+++ js/common.js
@@ -1,85 +1,18 @@
 $(document).ready(function(){
-	$(".js-phone").mask("+7 (999) 999 - 99 - 99?");
+
 	// Мобильное меню 
 	$(".fa-bars").click(function(m){
 		m.preventDefault();
-		$(".main-menu").slideToggle(500);
+		$(".header__menu-box").slideToggle(500);
 	});
 	// Конец Мобильное меню
 
-	// вывод блока в каталоге 
-	$(".js-button_1").click(function(z){
-		z.preventDefault();
-		$(".box2").slideToggle(500);
-	});
-	$(".js-button2").click(function(q){
-		q.preventDefault();
-		// $(".box2").css("display","none");
-		$(".box2").slideUp(500);
-	});
-	// Конец вывод блока в каталоге
 
-	/*MODAL WINDOW*/
-	$('a.js-modal').on("click", function(e){
-		e.preventDefault();
-		var  id = $(this).attr('href'),
-		winW = $(window).width(),
-		winH = $(window).height();
-		$(id).css("left", winW/2-$(id).width()/2);
-		$(id).css("top", winH/2-$(id).height()/2);
-		$('body').css({
-			"overflowY" : "hidden",
-			"paddingRight" : "17px"
-		});
-		$(id).fadeIn();
-		$('body').append('<div class="mask" id="js-mask"></div>');
-	});
 
 	
 
-	$('body').on("click", ".js-close", function(e){
-		e.preventDefault();
-		$('#js-mask').remove();
-		$('.js-window').hide();
-		$('body').removeAttr("style");
-	});
 
-	$('body').on("click", "#js-mask", function(){
-		$('#js-mask').remove();
-		$('.js-window').hide();
-		$('body').removeAttr("style");
 
-	});
-
-	/*MODAL SUBMIT*/
-	$('.js-submit').submit(function(){
-		var phone = $(this).find('input[name="phone"]');
-		
-		if(phone.val() == ""){
-			phone.focus();
-			return false;
-		}
-
-		else{
-			var form_data = $(this).serialize(); 
-			$.ajax({
-				type: "POST", 
-				url: "/message.php", 
-				data: form_data,
-				success: function(form) {	
-					$('.js-window').hide();
-					$("input[type=text]").val("");
-					$("input[type=email]").val("");
-					$("input[type=hidden]").val("");
-					$("textarea").val("");
-					$('#js-mask').remove();
-					$('a[href=#js-form2]').trigger('click');
-				// location = "thanks.php";
-			}
-		});
-		}
-		return false;
-	});
 
 	
 
