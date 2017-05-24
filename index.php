c82495b95a1776b23376c8ea88cc091574ebf821
Berzeg07 <berezg86@gmail.com>
2017-05-24 15:07:40 +0400

    29.14

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Главная</title>
+
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+	<!-- JS_BLOCK -->
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<script src="js/responsiveTabs.js"></script>
+	<script src="js/jquery.maskedinput.min.js"></script>
+	<script src="js/fonts.js"></script>
+	<script src="js/common.js"></script>
+
+
+</body>
+</html>
