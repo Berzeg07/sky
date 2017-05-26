9c30395482c9733101c73b72315ffadd8bed15ab
Berzeg07 <berezg86@gmail.com>
2017-05-26 17:10:31 +0400

    29.14

---
+++ partner.php
@@ -0,0 +1,449 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<title>Главная</title>
+
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/fonts.css">
+	<link rel="stylesheet" href="css/swiper.min.css">
+	<link rel="stylesheet" href="css/animated-slider.css" />
+	<style>
+		.swiper-container {
+			width: 100%;
+			padding-top: 50px;
+			padding-bottom: 50px;
+		}
+		.swiper-slide {
+			background-position: center;
+			background-size: cover;
+			position: relative;
+			width: 463px;
+			height: 400px;
+		}
+	</style>
+</head>
+<body>
+
+	<div class="header__wrap">
+		<div class="header__wrap_top">
+
+			<div class="container">
+				<div class="row">
+					<div class="header">
+
+						<div class="header__lang">
+							<div class="header__bars">
+								<i class="fa fa-bars" aria-hidden="true"></i>
+							</div>
+							<a class="lang_active" href="#">RU</a>
+							<a href="#">EN</a>
+						</div>
+						
+						<div class="header__menu-box">
+							<div class="col-xs-12 col-sm-12 col-md-6 no-padding">
+								<div class="header__tel">
+									<div class="header__tel_box"><a href="#"><img src="img/logo.png" alt=""></a></div>
+									<div class="header__tel_box header__tel_bottom"><span>8 800 800 80 80</span></div>
+									<div class="clearfix"></div>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-12 col-md-6 no-padding">
+								<ul class="header__menu">
+									<li><a href="#">главная</a></li>
+									<li><a href="#">семья</a></li>
+									<li><a href="#">друзья</a></li>
+									<li><a href="#">семьи</a></li>
+									<li><a href="#">контакты</a></li>
+								</ul>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="header__bottom">
+			<div class="container">
+				<span>Welcome</span>
+				<p>Главная страница сайта представляет собой лицо компании, дверь, через которую к вам входят клиенты. </p>
+			</div>	
+		</div>
+	</div>
+
+	
+	<div class="container dd">
+		<div class="row">
+
+			<div class="col-xs-12 col-sm-12 col-md-3 no-padding">
+				<a href="#">
+					<div class="block-logo block-logo_1">
+						<img class="img-responsive" src="img/im1.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="block-logo block-logo_2">
+						<img class="img-responsive" src="img/im2.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="block-logo block-logo_2">
+						<img class="img-responsive" src="img/im2.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+
+			<!-- 2 ===================== -->
+
+			<div class="col-xs-12 col-sm-12 col-md-3 no-padding">
+				<a href="#">
+					<div class="block-logo block-logo_2">
+						<img class="img-responsive" src="img/im2.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="block-logo block-logo_1">
+						<img class="img-responsive" src="img/im1.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="block-logo block-logo_3">
+						<img class="img-responsive" src="img/im3.jpg" alt="">
+						<div class="block-logo__hover block-logo__hover_3">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="block-logo block-logo_3">
+						<img class="img-responsive" src="img/im3.jpg" alt="">
+						<div class="block-logo__hover block-logo__hover_3">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+
+			<!-- 3 =================== -->
+
+			<div class="col-xs-12 col-sm-12 col-md-3 no-padding">
+				<a href="#">
+					<div class="block-logo block-logo_2">
+						<img class="img-responsive" src="img/im4.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="block-logo block-logo_1">
+						<img class="img-responsive" src="img/im1.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="block-logo block-logo_3">
+						<img class="img-responsive" src="img/im5.jpg" alt="">
+						<div class="block-logo__hover block-logo__hover_3">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+				<a href="#">
+					<div class="block-logo block-logo_3">
+						<img class="img-responsive" src="img/im5.jpg" alt="">
+						<div class="block-logo__hover block-logo__hover_3">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+			
+			<!-- 4 ===================== -->
+
+			<div class="col-xs-12 col-sm-12 col-md-3 no-padding">
+				<a href="#">
+					<div class="block-logo block-logo_4">
+						<img class="img-responsive" src="img/im6.jpg" alt="">
+						<div class="block-logo__hover block-logo__hover_3">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+
+				<a href="#">
+					<div class="block-logo block-logo_2">
+						<img class="img-responsive" src="img/im4.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+
+				<a href="#">
+					<div class="block-logo block-logo_2">
+						<img class="img-responsive" src="img/im4.jpg" alt="">
+						<div class="block-logo__hover">
+							<p>Описание товара в карточке под соответствующей картинкой рассчитаны обычно на 500-800 знаков.</p>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+
+	<!-- Swiper -->
+	<div class="container">
+		<h2 class="h2_slider">Привелегии</h2>
+		<div class="swiper-container">
+			<div class="swiper-wrapper">
+				<div class="swiper-slide">
+					<img src="img/sl1.jpg" alt="">
+					<div class="slider-txt">
+						<span>Месяц тест-драйва мазерати</span>
+					</div>
+				</div>
+				<div class="swiper-slide">
+					<img src="img/sl1.jpg" alt="">
+					<div class="slider-txt">
+						<span>Месяц тест-драйва мазерати</span>
+					</div>
+				</div>
+				<div class="swiper-slide">
+					<img src="img/sl1.jpg" alt="">
+					<div class="slider-txt">
+						<span>Месяц тест-драйва мазерати</span>
+					</div>
+				</div>
+				<div class="swiper-slide">
+					<img src="img/sl1.jpg" alt="">
+					<div class="slider-txt">
+						<span>Месяц тест-драйва мазерати</span>
+					</div>
+				</div>
+				<div class="swiper-slide">
+					<img src="img/sl1.jpg" alt="">
+					<div class="slider-txt">
+						<span>Месяц тест-драйва мазерати</span>
+					</div>
+				</div>
+				<div class="swiper-slide">
+					<img src="img/sl1.jpg" alt="">
+					<div class="slider-txt">
+						<span>Месяц тест-драйва мазерати</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- end slider -->
+	<div class="slider2-wrap">
+		<div class="container">
+			<div class="ah-slider">
+				<div class="container container_style2">
+					<div class="choose_slider">
+						<div id="btn_prev2"></div>
+						<div class="choose_slider_items">
+							<ul id="mySlider2">
+								<li class="current_item">
+									<div class="current_item1">
+										<div class="ah-slideimg">
+											<img src="img/plane.jpg" alt="ah-slider1">
+										</div>
+										<div class="play">
+											<a href="#"><img src="img/play.png" alt=""></a>
+										</div>
+										<div class="ah-current-wrapp">
+											<span class="item-title">Что нас ждет?</span>
+										</div>
+									</div>
+								</li>
+								<li class="current_item">
+									<div class="current_item1">
+										<div class="ah-slideimg">
+											<img src="img/plane.jpg" alt="ah-slider2">
+										</div>
+										<div class="play">
+											<a href="#"><img src="img/play.png" alt=""></a>
+										</div>
+										<div class="ah-current-wrapp">
+											<span class="item-title">Последние новости</span>
+										</div>
+									</div>			    
+								</li>
+								<li class="current_item">
+									<div class="current_item1">
+										<div class="ah-slideimg">
+											<img src="img/plane.jpg" alt="ah-slider3">
+										</div>
+										<div class="play">
+											<a href="#"><img src="img/play.png" alt=""></a>
+										</div>
+										<div class="ah-current-wrapp">
+											<span class="item-title">Что нас ждет?</span>
+										</div>
+									</div>			    
+								</li>
+								<li class="current_item">
+									<div class="current_item1">
+										<div class="ah-slideimg">
+											<img src="img/plane.jpg" alt="ah-slider4">
+										</div>
+										<div class="play">
+											<a href="#"><img src="img/play.png" alt=""></a>
+										</div>
+										<div class="ah-current-wrapp">
+											<span class="item-title">Что нас ждет?</span>
+										</div>
+									</div>                               
+								</li>
+								<li class="current_item">
+									<div class="current_item1">
+										<div class="ah-slideimg">
+											<img src="img/plane.jpg" alt="ah-slider5">
+										</div>
+										<div class="play">
+											<a href="#"><img src="img/play.png" alt=""></a>
+										</div>
+										<div class="ah-current-wrapp">
+											<span class="item-title">Что нас ждет?</span>
+										</div>
+									</div>                               
+								</li>
+								<li class="current_item">
+									<div class="current_item1">
+										<div class="ah-slideimg">
+											<img src="img/plane.jpg" alt="ah-slider1">
+										</div>
+										<div class="play">
+											<a href="#"><img src="img/play.png" alt=""></a>
+										</div>
+										<div class="ah-current-wrapp">
+											<span class="item-title">Что нас ждет?</span>
+										</div>
+									</div>                                
+								</li>
+							</ul>
+						</div>
+						<div id="btn_next2"></div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+	
+	<div class="enter-wrap">
+		<div class="container">
+			<div class="enter__message">
+				<h2>Вступайте в клуб Nebo Family</h2>
+				<p>Регистрируйтесь прямо сейчас и<br/>
+					используйте все привелегии!
+				</p>
+				<form class="form1" action="#" method="post">
+					<input class="enter-input" name="email" type="email" placeholder="Ваш email" required="">
+					<input class="enter-submit" name="submit" type="submit" value="вступить">
+					<div class="clearfix"></div>
+				</form>
+			</div>
+		</div>
+	</div>	
+
+	<div class="form-footer__wrap">
+		<div class="container">
+			<div class="form-footer">
+				<h2>Пожалуйста, не стесняйтесь написать<br/>
+					нам в любое время дня и ночи:<br/>
+					<b>8 800 800 80 80</b> 
+				</h2>
+				<div class="form-footer__box">
+					<form class="form1" action="#" method="post">
+						<input class="footer-input" name="email" type="email" placeholder="Ваш email" required="">
+						<input class="footer-input phone" name="phone" type="text" placeholder="Ваш телефон">
+						<input class="footer-submit" name="submit" type="submit" value="оставить заявку">
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="social">
+			<ul>
+				<li><a href="#"><img src="img/vk.png" alt=""></a></li>
+				<li><a href="#"><img src="img/face.png" alt=""></a></li>
+				<li><a href="#"><img src="img/twit.png" alt=""></a></li>
+				<li><a href="#"><img src="img/in.png" alt=""></a></li>
+			</ul>
+		</div>
+		<div class="copyright">
+			<div class="copyright-box_left"><img src="img/img1.png" alt=""></div>
+			<div class="copyright-box_right">
+				<p>
+					Copyright © Webrazavr. 2017. All rights reserved<br/>
+					Любое использование материалов, представленных на сайте,<br/>
+					разрешено только с письменного согласия владельца. 
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- JS_BLOCK -->
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/owl.carousel/owl.carousel.js"></script>
+	<!-- <script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+	<script src="js/responsiveTabs.js"></script> -->
+	<script src="js/jquery.maskedinput.min.js"></script>
+	<script src="js/fonts.js"></script>
+	<script src="js/common.js"></script>
+	<script src="js/swiper.min.js"></script>
+	<script src="js/jquery.cssslider.js"></script>
+
+	<script>
+		
+</script>
+
+<div id="mask"></div>
+<div id="thanks" class="window">
+	<div class="form-wrap-main">
+		<div class="dd-close">X</div>
+		<div class="form-title">
+			Спасибо за заявку!
+		</div>
+		<div class="dd-form-txt">
+			Наш менеджер свяжется с вами в ближайшее время!
+		</div>
+	</div>
+</div>
+<a href="#thanks" name="modal"></a>
+
+</body>
+</html>
